206b8c342851d9f412062627a5f03780ef9841ba
pawarsourabh2806-ux <pawarsourabh2806@gmail.com>
2026-01-22 18:38:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,77 +2,65 @@ let userscore = 0;
 let comoscore = 0;
 
 const choices = document.querySelectorAll(".choice");
-// console.log(choices); 
-const msg= document.querySelector(".msg");
-const userscore_para =document.querySelector(".user-score");
+const msg = document.querySelector(".msg");
+const userscore_para = document.querySelector(".user-score");
 const comscore_para = document.querySelector(".com-score");
 
+// DRAW GAME
+const drowgame = (userchice) => {
+  msg.innerText = `Your choice ${userchice}. Game Draw! Play again.`;
+  msg.style.backgroundColor = "black";
+};
 
-const drowgame =()=>{
-//  console.log("game was drow")
- msg.innerText=`your choice ${userchice}  Game draw play again!`;
-   msg.style.backgroundColor="black";
-}
-
-const showwinner = (userwin, userchice, comchoice) =>{
-if(userwin){
-    // console.log("you win");
+// SHOW WINNER
+const showwinner = (userwin, userchice, comchoice) => {
+  if (userwin) {
     userscore++;
-    userscore_para.innerText=userscore;
-    msg.innerText=`you win your ${userchice} beats ${comchoice}`;
-    msg.style.backgroundColor="green"; 
-}
-else{
-    // console.log("you lose");
-     comoscore++;
-     comscore_para.innerText=comoscore;
-    msg.innerText=`you lost ${comchoice} beat your ${userchice}`;
-    msg.style.backgroundColor="red"; 
-}
-}
+    userscore_para.innerText = userscore;
+    msg.innerText = `You win! Your ${userchice} beats ${comchoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    comoscore++;
+    comscore_para.innerText = comoscore;
+    msg.innerText = `You lost! ${comchoice} beats your ${userchice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
 
+// COMPUTER CHOICE
+const gencompchoice = () => {
+  const option = ["ston", "paper", "scissor"];
+  const ranidx = Math.floor(Math.random() * 3);
+  return option[ranidx];
+};
 
-const gencompchoice =()=>{
-    const option =["ston","paper","scissor"]
-    const ranidx=Math.floor(Math.random()*3);
-    return option[ranidx]
-}
+// MAIN GAME LOGIC
+const playgame = (userchice) => {
+  const comchoice = gencompchoice();
 
-const playgame =(userchice)=>{
-    // console.log("user choice",userchice);
+  if (userchice === comchoice) {
+    drowgame(userchice);
+  } else {
+    let userwin;
 
-    const comchoice =gencompchoice();
-//    console.log("computer choice", comchoice);
-   
-   if(userchice === comchoice){
-    //drow game
-    drowgame();
-   }
-   else{
-    let userwin = true;
-    if(userchice ==="paper"){
-        userwin = comchoice ==="ston"? true :false;   
+    if (userchice === "paper") {
+      userwin = comchoice === "ston";
+    } 
+    else if (userchice === "scissor") {
+      userwin = comchoice === "paper";
+    } 
+    else { // ston
+      userwin = comchoice === "scissor";
     }
-    else if(userchice==="scissor"){
-        userwin = comchoice ==="ston"? false :true; 
-    }
-    else if(userchice==="ston"){
-        userwin = comchoice ==="scissor"? true :false; 
-    }
-     
-    showwinner(userwin, userchice,comchoice)
-    }
-    
-}
 
+    showwinner(userwin, userchice, comchoice);
+  }
+};
 
-choices.forEach((choice)=>{
-    choice.addEventListener("click",()=>{
-        const userchice= choice.getAttribute("id")
-        // console.log("choice is clicked",userchice);
-       playgame(userchice) 
-
-    })
-    
-})
-
+// CLICK EVENTS
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userchice = choice.getAttribute("id");
+    playgame(userchice);
+  });
+});
